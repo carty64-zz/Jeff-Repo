b657d46412aad20bee30175434fc971815416294
Jeff Cartwright <cartwright.jeff@gmail.com>
2016-05-14 22:34:27 -0700

    initial commit

---
+++ bits.py
@@ -0,0 +1,47 @@
+from coinbase.wallet.client import Client
+
+SANDBOX_URL = 'https://api.sandbox.coinbase.com'
+client = Client('XkeNVpuPu8gg6ooT','AzOlNhddoZQaKjaEw2QXEjaO1I8Gqpgs',base_api_uri=SANDBOX_URL)
+
+accounts = client.get_accounts()
+wallets = []
+
+for index,account in enumerate(accounts.data):
+  name = account['name']
+  balance = account.balance
+  wallets.append(name)
+  print "%i) %s:  %s" % (index+1, name, balance)  
+
+src = int(raw_input("From account? "))-1
+dest = int(raw_input("To account? "))-1
+amount = raw_input("Amount: ")
+amount = float(amount)
+
+src_id = accounts[src]['id']
+dest_id = accounts[dest]['id']
+
+tx = client.transfer_money(src_id,dest_id,amount)
+
+print tx
+
+
+#print wallets[src]
+#print amount
+#print wallets[dest]
+
+#try:
+#  while(1):
+#first = raw_input('From acct: 1)%s', wallets)
+
+#for account in accounts.data:
+#  print account.name
+
+
+#for account in accounts.data:
+#  balance = account.balance
+#  wallet_id = account.id
+#  print "%s: %s %s (%s)" % (account.name, balance.amount, balance.currency, wallet_id)
+#  print account.get_transactions()
+
+#print accounts.data
+
