ee23725c55a8e1f2179366e7a64c0e0f625604d4
Jeff Cartwright <cartwright.jeff@gmail.com>
2016-06-04 16:21:06 -0700

    update weather temp

--- weatherman.py
+++ weatherman.py
@@ -19,12 +19,12 @@ try:
 #        print temp < 56
 #        print 56 < temp < 67
 #        print temp >= 67
-        if temp < 56.0:
+        if temp < 57.0:
             GPIO.setup(11, GPIO.OUT)
             GPIO.output(11, True)
             time.sleep(600)
             GPIO.output(11, False)
-        elif 56.0 < temp < 67.0:
+        elif 57.0 < temp < 67.0:
             GPIO.setup(12, GPIO.OUT)
             GPIO.output(12, True)
             time.sleep(600)
