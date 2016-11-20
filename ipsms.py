f7d4ee917d60abf8391414c5b1296e3a39b858d9
Jeff Cartwright <cartwright.jeff@gmail.com>
2016-11-20 11:59:22 -0800

    added SSID

--- ipsms.py
+++ ipsms.py
@@ -2,14 +2,16 @@ from twilio.rest import TwilioRestClient
 import os
 from subprocess import *
 
+#phone number format "+18009998888"
 ACCOUNT_SID = os.environ["TWILIO_SID"]
-AUTH_TOKEN = os.environ["TWILIO_AUTH"]
+AUTH_TOKEN = os.environ["TWILIO_API"]
 me = os.environ["PHONE"]
 twilio = os.environ["TWILIO_NUM"]
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 
 ip = "ip addr show wlan0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1"
+ssid = 'iwconfig wlan0 | grep "ESSID" | cut -d: -f2'
 device = "hostname"
 def get_device(device):
         p = Popen(device, shell=True, stdout=PIPE)
@@ -21,7 +23,12 @@ def get_ip(ip):
         output = p.communicate()[0]
         return output
 
-text_message = (get_device(device)+get_ip(ip))
+def get_ssid(ssid):
+        p = Popen(ssid, shell=True, stdout=PIPE)
+        output = p.communicate()[0]
+        return output
+
+text_message = (get_device(device)+get_ssid(ssid)+get_ip(ip))
 
 message = client.messages.create(
     to=me, 
