2d9617f135ba4adc66501308434c4746c76610d4
Jeff Cartwright <cartwright.jeff@gmail.com>
2017-03-14 23:21:07 -0700

    update

--- getgames.py
+++ getgames.py
@@ -6,14 +6,14 @@ import time
 
 print time.time()
 todays_date =  datetime.date.today().strftime("%Y%m%d")
-url = 'http://data.nba.com/data/json/nbacom/2016/gameline/%s/games.json' % (todays_date)
-#url = 'http://data.nba.com/data/json/nbacom/2015/gameline/20160325/games.json'
+#url = 'http://data.nba.com/data/json/nbacom/2016/gameline/%s/games.json' % (todays_date)
+url = 'http://stats.nba.com/scores/GameID=0041400235&GameEventID;=308'
 response = urllib2.urlopen(url)
 data = response.read()
-data = json.loads(data)
+#data = json.loads(data)
 print time.time() 
 
-#print data
+print data
 
 #use this to load a local JSON file
 #file = open('gamedata.json', 'r')
@@ -21,12 +21,12 @@ print time.time()
 #gamedata = json.loads(data, encoding="utf-8")
 #print gamedata
 
-game_ids = []
+#game_ids = []
 
-for games in data['games']:
-  for teams in games['teams']:
-    game_ids.append(str(teams['scores']))
-    print teams['scores']
+#for games in data['games']:
+#  for teams in games['teams']:
+#    game_ids.append(str(teams['scores']))
+#    print teams['scores']
 
 #for x in game_ids:
 #  print x
@@ -46,14 +46,14 @@ for games in data['games']:
 #data = response.content
 #nbadata = json.loads(data, encoding="utf-8")
 
-matchup = []
-results = []
+#matchup = []
+#results = []
 
-for x in data['games']:
-  games = x['teams']
-  for z in games:
-    matchup.append(str(z.get('awayTeam') or z.get('homeTeam')))
-    results.append(z['scores'][0])
+#for x in data['games']:
+#  games = x['teams']
+#  for z in games:
+#    matchup.append(str(z.get('awayTeam') or z.get('homeTeam')))
+#    results.append(z['scores'][0])
 
-for c1, c2 in zip(matchup, results):
-  print c1, c2
+#for c1, c2 in zip(matchup, results):
+#  print c1, c2
