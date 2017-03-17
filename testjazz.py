49cd1d533de8b2d46c717dfb8c2bea9e33a2b806
Jeff Cartwright <cartwright.jeff@gmail.com>
2017-03-16 20:18:36 -0700

    fixes

--- testjazz.py
+++ testjazz.py
@@ -6,17 +6,20 @@ token = os.environ["JAZZBOT_KEY"]
 
 sc = SlackClient(token)
 
+def scores():
+  sc.api_call(
+    "chat.postMessage", channel="jazzbot_test", text="here are the scores!", as_user = 'true'
+  )
+
 if sc.rtm_connect():
     while(1):
       message = sc.rtm_read()
       print message
       time.sleep(1)
-#    if message == "scores":
-#      scores()
+      if len(message) > 0 and message[0].has_key('type') and message[0]['type'] == 'message': 
+        if message[0]['text'] == "scores":
+          scores()  
+
 else:
   print "Connection Failed, invalid token?"
 
-def scores():
-  sc.api_call(
-    "chat.postMessage", channel="jazzbot_test", text="here are the scores!", as_user = 'true'
-  )
