6625bcd0d282e9d367fdbce2c52a5bfc29342591
Jeff <cartwright.jeff@gmail.com>
2017-04-16 15:42:23 -0700

    all the updates

---
+++ jazzmotion.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from gpiozero import MotionSensor
+from datetime import datetime
+from time import time, sleep
+from picamera import PiCamera, Color
+import sqlite3
+from slackclient import SlackClient
+import os
+
+#xoxb-31174033648-jkTfRMnKNfCF931gH8CIdVD0
+token = os.environ["JAZZBOT_KEY"]
+conn = sqlite3.connect('monitoring.db')
+c = conn.cursor()
+
+sc = SlackClient(token)
+
+camera = PiCamera()
+camera.framerate = 60
+pir = MotionSensor(17)
+camera.resolution = (800,600)
+camera.rotation = 90
+camera.annotate_background = Color('black')
+camera.hflip = True
+
+
+if sc.rtm_connect():
+
+  while True:
+    pir.wait_for_motion()
+    sleep(0.5)
+    filename = datetime.now().strftime("%Y-%m-%d_%H.%M.%S")
+    camera.annotate_text = filename
+    camera.capture('/home/pi/Desktop/Motion_Pics/%s.jpg' % filename)
+#    camera.start_recording('/home/pi/Desktop/Motion_Vids/%s.h264' % filename)
+    try:
+      c.execute("INSERT INTO motion VALUES ('bedroom',date(),time())")
+      conn.commit()
+    except Exception as e:
+      print e
+
+    try:
+      sc.api_call(
+       'files.upload',
+       channels="#jazzbot_test",
+       filename='Motion Detected',
+       file=open('/home/pi/Desktop/Motion_Pics/%s.jpg' % filename, 'rb')
+      )
+    except Exception as e:
+      print e
+
+#    pir.wait_for_no_motion()
+#    camera.stop_recording()
+
+else:
+  print "bad connection"
