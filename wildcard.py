2144bd4970974ad3c2699fd748498da1f7a221f7
Jeff Cartwright <cartwright.jeff@gmail.com>
2017-11-19 12:46:40 -0800

    shrug

--- wildcard.py
+++ wildcard.py
@@ -3,11 +3,16 @@
 import os
 import random
 
+
 def wildcard(member_names):
   x = random.randint(0,len(member_names)-1)
   name = member_names[x]
+  name = pickRandom()
   print name
   return name
+
+class pickRandom(name):
+  pass
 
 if __name__ == "__wildcard__":
   wildcard()
