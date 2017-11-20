02b1d11ccf0a889766012b8708a93c899c1a8788
Jeff Cartwright <cartwright.jeff@gmail.com>
2017-11-19 18:15:24 -0800

    lots of fun changes! Now it actually works

--- chatbot.py
+++ chatbot.py
@@ -13,17 +13,17 @@ token = os.environ["CHAT_KEY"]
 sc = SlackClient(token)
 chatbot = '<@U7F2D8W90>'
 
-channel_info = sc.api_call(
-    "groups.info",channel = 'G0XFPT083')
-
-member_ids = channel_info['group']['members']
+#channel_info = sc.api_call(
+#    "groups.info",channel = 'G0XFPT083')
+#
+#member_ids = channel_info['group']['members']
 #print member_ids
-member_names = []
+#member_names = []
 
-for member_id in member_ids:
-  user_info = sc.api_call("users.info",user = member_id)
-  if user_info['user']['deleted'] == False and user_info['user']['is_bot'] == False:
-    member_names.append("<@" + user_info['user']['name'] + ">")
+#for member_id in member_ids:
+#  user_info = sc.api_call("users.info",user = member_id)
+#  if user_info['user']['deleted'] == False and user_info['user']['is_bot'] == False:
+#    member_names.append("<@" + user_info['user']['name'] + ">")
 
 if sc.rtm_connect():
   while True:
@@ -32,11 +32,12 @@ if sc.rtm_connect():
     sleep(1)
     if len(message) > 0 and message[0].has_key('type') and message[0]['type'] == 'message':
       text = message[0]['text']
-      if chatbot in text:
+      if text.startswith(chatbot) == True:
         text = text.lower()
-        if 'random' in text:
-          wildcard.wildcard(member_names)
-	  print wildcard.name()	
+        if 'random' in text and message[0]['channel'].startswith('D') == False:
+          sender = message[0]['user']
+          channel = message[0]['channel']
+          wildcard.wildcard(sender,channel)
         if 'smile' in text:
           sc.api_call("chat.postMessage", channel="jazzbot_test", text=":smile:", as_user = 'true')
 
--- wildcard.py
+++ wildcard.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python
 
+from slackclient import SlackClient
 import os
 import random
 
+token = os.environ["CHAT_KEY"]
+sc = SlackClient(token)
+chatbot = '<@U7F2D8W90>'
 
-def wildcard(member_names):
+def wildcard(sender,channel):
+  if channel.startswith('C') == True:
+      channel_info = sc.api_call("channels.info", channel=channel)
+      member_ids = channel_info['channel']['members']
+  elif channel.startswith('G') == True:
+      channel_info = sc.api_call("groups.info",channel=channel)
+      member_ids = channel_info['group']['members']
+  member_names = []
+  for member_id in member_ids:
+    user_info = sc.api_call("users.info",user = member_id)
+    member_names.append("<@" + user_info['user']['name'] + ">")
+# Inital group only had bots and me, so ignoring bots left an empty list
+# should create an error for this, which would apply in a DM
+#    if user_info['user']['deleted'] == False and user_info['user']['is_bot'] == False:
+#      member_names.append("<@" + user_info['user']['name'] + ">")
+  sender = sc.api_call("users.info",user = sender)
+  sender = "<@" + sender['user']['name'] + ">"
+  pos = member_names.index(sender)
+  del member_names[pos]
   x = random.randint(0,len(member_names)-1)
   name = member_names[x]
-  name = pickRandom()
-  print name
-  return name
-
-class pickRandom(name):
-  pass
+  sc.api_call("chat.postMessage", channel=channel, text="How about "+name+"?", as_user = 'true')
 
 if __name__ == "__wildcard__":
   wildcard()
