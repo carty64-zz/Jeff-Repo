edc280da126350a5c89a575590f3276633daa071
Jeff Cartwright <cartwright.jeff@gmail.com>
2017-11-20 22:31:04 -0800

    no response if no valid users to choose from

--- wildcard.py
+++ wildcard.py
@@ -18,5 +18,6 @@ def wildcard(sender, channel):
         raise ValueError("Unknown channel type")
     member_info = [slack_client.api_call('users.info', user=member_id)['user'] for member_id in channel_info['members'] if member_id != sender]
     member_names = [info['name'] for info in member_info if not (info['deleted'] or info['is_bot'])]
-    random_name = "<@"+random.choice(member_names)+">"
-    slack_client.api_call('chat.postMessage', channel=channel, text="How about %s?" % random_name, as_user=True)
+    if len(member_names) > 0:    
+      random_name = "<@"+random.choice(member_names)+">"
+      slack_client.api_call('chat.postMessage', channel=channel, text="How about %s?" % random_name, as_user=True)
